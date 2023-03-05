da9c3e7202c28f16925b3f9c8ddaec0aae4b66e6
carloslima <T3549833@timbrasil.com.br>
2023-03-05 18:32:56 -0300

    criado data classes do objeto characters + camada remote

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -1,7 +1,16 @@
 package com.example.marvelapp.framework.network.response
 
+import com.example.core.domain.model.CharacterItem
+
 data class CharacterResponse(
     val id: Int,
     val name: String,
     val thumbnail: ThumbnailResponse
-)
+){
+    fun toCharacterModel() : CharacterItem =
+        CharacterItem(
+            name = this.name,
+            imageUrl = "${this.thumbnail.path}.${this.thumbnail.extension}"
+        )
+}
+
